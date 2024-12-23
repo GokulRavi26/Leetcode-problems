a60b6de426526d6f0545295256b2ff4cb5ecbce5
GokulRavi26 <148682571+GokulRavi26@users.noreply.github.com>
2024-12-23 15:15:20 +0530

    Time: 45 ms (89.04%), Space: 64 MB (71.92%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -1,57 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-
-    private int indexOf(int[] arr, int ele) {
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] == ele)
-                return i;
-
-        return -1;
-    }
-
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    private int minSwaps(int[] arr, int N) {
-        int ans = 0;
-        int[] temp = Arrays.copyOfRange(arr, 0, N);
-        Arrays.sort(temp);
-
-        for (int i = 0; i < N; i++) {
-            if (arr[i] != temp[i]) {
-                ans++;
-                swap(arr, i, indexOf(arr, temp[i]));
-            }
-        }
-        return ans;
-    }
-
     public int minimumOperations(TreeNode root) {
+        int count = 0;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int count = 0;
-
-        while (!q.isEmpty()) {
-            TreeNode curr = q.peek();
+        while(!q.isEmpty()) {
             int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                curr = q.remove();
-                if (curr.left != null)
-                    q.add(curr.left);
-
-                if (curr.right != null)
-                    q.add(curr.right);
+            int[] arr1 = new int[size];
+            int[] arr2 = new int[size];
+            for(int i = 0; i < size; i++) {
+                TreeNode temp = q.poll();
+                arr1[i] = temp.val;
+                arr2[i] = temp.val;
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
             }
+            count += fun(arr1, arr2);
+        }
+        return count;
+    }
 
-            int[] arr = new int[q.size()];
-            int k = 0;
-            for (TreeNode num : q)
-                arr[k++] = num.val;
-
-            count += minSwaps(arr, k);
+    static int fun(int[] arr1, int[] arr2) {
+        int n = arr1.length;
+        if(n == 1) return 0;
+        Arrays.sort(arr1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++) map.put(arr1[i], i);
+        
+        int count = 0;
+        for(int i = 0; i < n; ) {
+            int index = map.get(arr2[i]);
+            if(index == i) i++;
+            else {
+                count++;
+                int temp = arr2[i];
+                arr2[i] = arr2[index];
+                arr2[index] = temp;
+            }
         }
         return count;
     }
