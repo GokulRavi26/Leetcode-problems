372a8c4b83c94837c8aa695d49d55c45d46d335c
GokulRavi26 <148682571+GokulRavi26@users.noreply.github.com>
2025-07-19 12:59:14 +0530

    Time: 41 ms (77.54%), Space: 54.5 MB (95.12%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.java
@@ -1,40 +1,21 @@
+import java.util.*;
+
 class Solution {
-  class Node {
-    Map<String, Node> next = new HashMap<>();
-    boolean isEnd;
-  }
-  Node root = new Node();
-
-  public List<String> removeSubfolders(String[] folder) {
-    for (var path : folder) {
-      var p = root;
-
-      for (var dir : path.split("/")) {
-        if (dir.equals("")) continue;
-
-        if (!p.next.containsKey(dir))
-          p.next.put(dir, new Node());
-
-        p = p.next.get(dir);
-      }
-      p.isEnd = true;
-    }
-    var res = new ArrayList<String>();
-
-    for (var path : folder) {
-      var p = root;
-      var isValid = true;
-
-      for (var dir : path.split("/")) {
-        if (dir.equals("")) continue;
-        if (p.isEnd) {
-          isValid = false;
-          break;
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> res = new ArrayList<>();
+        for (String f : folder) {
+            if (res.isEmpty()) {
+                res.add(f);
+            } else {
+                String prev = res.get(res.size() - 1);
+                if (f.startsWith(prev) && f.length() > prev.length() && f.charAt(prev.length()) == '/') {
+                    continue;
+                } else {
+                    res.add(f);
+                }
+            }
         }
-        p = p.next.get(dir);
-      }
-      if (isValid) res.add(path);
+        return res;
     }
-    return res;
-  }
 }
