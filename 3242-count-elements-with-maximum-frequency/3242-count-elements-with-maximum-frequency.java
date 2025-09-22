fdb3e7ed45cf48721ca8472a79206f7f48a4c679
GokulRavi26 <148682571+GokulRavi26@users.noreply.github.com>
2025-09-22 11:49:00 +0530

    Time: 1 ms (99.44%), Space: 42.4 MB (56.53%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,35 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        int n = nums.length;
-        int freq[] = new int[n];
-        int visisted[] = new int[n];
-        int totalfreq = 0;
-        for(int i=0;i<n;i++){
-            if(visisted[i] == 1){
-                continue;
-            }
-
-            int count = 1;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                    visisted[i] = 1;
-                }
-            }
-
-            freq[i] = count;
+        int[] freq = new int[101];  // numbers are in range 1–100
+        for (int num : nums) {
+            freq[num]++;
         }
-         
-         int max = 0;
-         for(int value : freq){
-             max = Math.max(max,value);
-         }
 
-         for(int value : freq){
-             if(value == max){
-                 totalfreq +=value;
-             }
-         }
-        return totalfreq;
+        int maxFreq = 0;
+        for (int f : freq) {
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int result = 0;
+        for (int f : freq) {
+            if (f == maxFreq) {
+                result += f;
+            }
+        }
+
+        return result;
     }
 }
