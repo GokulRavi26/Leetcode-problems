7a256837fa4442d58da987486f9843b399a94317
GokulRavi26 <148682571+GokulRavi26@users.noreply.github.com>
2025-11-21 11:55:13 +0530

    Time: 122 ms (88.75%), Space: 46.2 MB (34.04%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -1,21 +1,38 @@
 class Solution {
-  public int countPalindromicSubsequence(String s) {
-    int ans = 0;
-    int[] first = new int[26];
-    int[] last = new int[26];
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, n);
+        Arrays.fill(last, -1);
 
-    Arrays.fill(first, s.length());
+        // find first & last
+        for (int i = 0; i < n; i++) {
+            int c = s.charAt(i) - 'a';
+            first[c] = Math.min(first[c], i);
+            last[c] = i;
+        }
 
-    for (int i = 0; i < s.length(); ++i) {
-      final int index = s.charAt(i) - 'a';
-      first[index] = Math.min(first[index], i);
-      last[index] = i;
+        int res = 0;
+
+        // for each character as the outer sides
+        for (int c = 0; c < 26; c++) {
+            int L = first[c];
+            int R = last[c];
+            if (R - L < 2) continue; // no space inside
+            
+            boolean[] memo = new boolean[26];
+
+            // scan only between L and R
+            for (int i = L + 1; i < R; i++) {
+                int idx = s.charAt(i) - 'a';
+                if (!memo[idx]) {
+                    memo[idx] = true;
+                    res++;
+                }
+            }
+        }
+
+        return res;
     }
-
-    for (int i = 0; i < 26; ++i)
-      if (first[i] < last[i])
-        ans += s.substring(first[i] + 1, last[i]).chars().distinct().count();
-
-    return ans;
-  }
 }
